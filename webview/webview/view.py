df775695ec39808903bfd498090071b152b1dcf7
DoubleMice <doublemice@qq.com>
2018-12-27 17:03:32 +0800

    :sparkles:添加空日志页面

--- webview/webview/view.py
+++ webview/webview/view.py
@@ -5,10 +5,13 @@ def hello(request):
     LOG = "/tmp/ngx_unsafe_log"
     print(LOG)
     log_file = os.listdir(LOG)
-    logs = open(os.path.join(LOG,log_file[0]),"r")
-    content = []
-    for log in logs:
-        content.append(json.loads(log))
     context = {}
-    context["logs"] = content
-    return render(request, 'index.html', context)
+    if log_file == []:
+        return render(request, 'null.html',context)
+    else:
+        logs = open(os.path.join(LOG,log_file[0]),"r")
+        content = []
+        for log in logs:
+            content.append(json.loads(log))
+        context["logs"] = content
+        return render(request, 'index.html', context)
